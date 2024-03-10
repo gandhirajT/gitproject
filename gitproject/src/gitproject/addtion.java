841381057a7df7c2c5ab9c8cc352f0aa1635c85b
vishali_trainee <vishali@example.com>
2024-03-10 18:55:48 +0530

    first commit....

---
+++ gitproject/src/gitproject/addtion.java
@@ -0,0 +1,15 @@
+package gitproject;
+
+public class addtion {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=2;
+		b=34;
+		c=a+b;
+		System.out.println(c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
